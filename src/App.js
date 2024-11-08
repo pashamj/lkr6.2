920c3dcfd664deaf13712fd320a597bd36218f46
Pavel Mejinschii <pmezhinsky@bk.ru>
2024-11-08 20:34:56 +0300

    11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from "react";
+import { Link, Routes, Route } from "react-router-dom";
+
+const MainPage = () => <div>Content of main page</div>;
+const Catalog = () => <div>Content of catalog</div>;
+const Contacts = () => <div>Content of contacts</div>;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <h3>Меню</h3>
+                <ul>
+                    <li><Link to="/">Главная страница</Link></li>
+                    <li><Link to="/catalog">Каталог</Link></li>
+                    <li><Link to="/contacts">Контакты</Link></li>
+                </ul>
+            </div>
+            <div>
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/catalog" element={<Catalog />} />
+                    <Route path="/contacts" element={<Contacts />} />
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
 export default App;
